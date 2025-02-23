0f597052511bfa280fa878fc33fd7a017f09f4fa
yazhouma <yazhouma3@creditease.cn>
2025-02-23 17:52:21 +0800

    初始化

---
+++ src/main/java/com/asia/App.java
@@ -0,0 +1,13 @@
+package com.asia;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
